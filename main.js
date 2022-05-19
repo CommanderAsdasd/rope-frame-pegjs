cfa240f2d78b5bfb28bb3ceb0355e44b7b93cd28
Commander asdasd <atank@kd-a5f4e0cc-3385-11b2-a85c-dff13eaabae1.ibm.com>
2022-05-19 11:18:52 +0200

    parser loads from nodejs module

--- main.js
+++ main.js
@@ -1,15 +1,19 @@
 
 const fs = require('fs');
 const pegjs = require('pegjs')
+const parser_lib = require('./public/rope-frame-a1.js')
+
 
 
 let parser_text = fs.readFileSync('public/rope-frame-a.pegjs', 'utf8')
 let parser = pegjs.generate(parser_text)
 let result = parser.parse("(a)")
+let result = parser_lib.parse("(a)")
 
 let ropes = {}
 let frames = {};
 
 console.log('parsed result: ', result)
+console.log('parsed result: ', result[0]['value'][0]['value'])
 console.log('ropes: ', ropes)
 console.log('frames: ', frames)
